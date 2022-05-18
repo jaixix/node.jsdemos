8b83ce6888828a002d6dc06fee11242a17eee8e9
Jaideep Lalchandani <jlalchandani@BLR.ALLEGISINDIA.COM>
2022-05-18 11:38:30 +0530

    changes

--- 01-intro/05-appchalk.js
+++ 01-intro/05-appchalk.js
@@ -2,4 +2,4 @@ import chalk from 'chalk'
 
 console.log(chalk.green('Success....'));
 console.log(chalk.redBright('Error Occured....'));
-console.log(chalk.green.inverse.bold('Backgroud Color with inverse'));
+console.log(chalk.green.inverse.bold('Backgroud Color with inverse') + ".");
--- 01-intro/06-appweatherapi.js
+++ 01-intro/06-appweatherapi.js
@@ -2,11 +2,21 @@
 
 //http://api.weatherstack.com/current?access_key=YOUR_ACCESS_KEY&query=Lucknow
 
-import request from 'request'
+import request from 'request';
+import chalk from 'chalk';
+import validator from 'validator';
+
 const url = 'http://api.weatherstack.com/current?access_key=9b9e6a890726f0d1e867eda769c7d8c1&query=Lucknow';
 
 request({url: url, json:true}, (error, response) => {
-    // console.log(response.body);
-    console.log(response.body.current.weather_descriptions + ". It is currently : " + 
-    response.body.current.temperature + "'C in Lucknow!");
+    if(validator.isURL(url)){
+        console.log(chalk.green(response.body.current.weather_descriptions[0]) +
+        ". It is currently : " + 
+        chalk.yellow.inverse(response.body.current.temperature + "'C") + " in " + 
+        chalk.redBright(response.body.location.name + ", " +
+        response.body.location.region));
+    }
+    else{
+        console.log("Invalid URL!");
+    }
 })
