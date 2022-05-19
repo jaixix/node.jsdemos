afb8b9742a9bd6981c3e62a072e27d4c73eea9b4
Jaideep Lalchandani <jlalchandani@BLR.ALLEGISINDIA.COM>
2022-05-19 11:01:59 +0530

    changes

--- 02-webapp/src/app.js
+++ 02-webapp/src/app.js
@@ -1,16 +1,44 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 // import request from 'request';
 const app = express();
 
-const url = 'http://api.weatherstack.com/current?access_key=9b9e6a890726f0d1e867eda769c7d8c1&query=Lucknow';
-
-
 const publicDirectoryPath = path.join(__dirname, '../public');
-console.log(publicDirectoryPath);
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
-// app.set('view engine','hbs');
+console.log(publicDirectoryPath);
+console.log(partialsPath);
+console.log(viewsPath);
+
+app.set('view engine','hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+      title: 'Weather Report',
+      name: 'Jaideep Lalchandani',
+    });
+  });
+
+app.get('/about', (req,res) => {
+    res.render('about',{
+        title : "About Us Dynamic",
+        name : "Jaideep Lalcandani",
+        author: "Picasso"
+    });
+});
+
+app.get('/help', (req,res) => {
+    res.render('help',{
+        helptext : "Welcome to the product suport page!",
+        title : "Help Page Dynamic",
+        name : "Jaideep Lalcandani"
+    });
+});
 
 app.get('/weather', (res,req) => {
     // request({url: url, json:true}, (error, response) => {
@@ -21,7 +49,10 @@ app.get('/weather', (res,req) => {
     //         response.body.location.region);
     //         console.log(s);
     // });
-    res.send("Weather API!");
+    res.send({
+        forecast: 'It is cloudy',
+        location: 'Lucknow',
+      });
 });
 
 app.listen(8081, () => {
